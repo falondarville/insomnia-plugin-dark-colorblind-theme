0aea5f94c129325a039e855698816eef82dcc60f
Falon Darville <falondarville@gmail.com>
2021-09-29 17:23:32 -0700

    init

---
+++ themes/dark-colorblind.js
@@ -0,0 +1,32 @@
+module.exports = {
+	name: 'Dark Colorblind',
+	displayName: 'Dark Colorblind',
+	theme: {
+		background: {
+			default: '#F0F6FC1A',
+			success: '#1F6FEB',
+			notice: '#F1E05A',
+			warning: '#D2A8FF',
+			danger: '#E34C26',
+			surprise: '#D29922',
+			info: '#58A6FF'
+		},
+		styles: {
+			dialog: {
+				background: {
+					default: '#F0F6FC1A'
+				}
+			},
+			paneHeader: {
+				background: {
+					success: '#1F6FEB',
+					notice: '#F1E05A',
+					warning: '#D2A8FF',
+					danger: '#E34C26',
+					surprise: '#D29922',
+					info: '#58A6FF'
+				}
+			},
+		}
+	}
+}
