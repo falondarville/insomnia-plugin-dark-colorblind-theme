2f94407e575845375aceb3fc82c08b0e31b451d3
Falon Darville <falondarville@gmail.com>
2021-09-30 21:15:02 -0700

    add primary color and deep-dive comments

--- themes/dark-colorblind.js
+++ themes/dark-colorblind.js
@@ -3,30 +3,34 @@ module.exports = {
 	displayName: 'Dark Colorblind',
 	theme: {
 		background: {
-			default: '#F0F6FC1A',
-			success: '#1F6FEB',
-			notice: '#F1E05A',
-			warning: '#D2A8FF',
-			danger: '#E34C26',
-			surprise: '#D29922',
-			info: '#58A6FF'
+			default: '#0D1117', 	// primary background color
+			success: '#1F6FEB', 	// POST request, parameter names
+			notice: '#E8F086', 		// SEND button, GET request
+			warning: '#A691AE', 	// PUT request
+			danger: '#FF4242', 		// DELETE request
+			surprise: '#FFC20A', 	// accent (Dashboard link, branch button, add plugin button)
+			info: '#58A6FF' 		// OPTIONS AND HEAD request
+		},
+		foreground: {
+			// font colors corresponding with the elements above?
+			// there's like 5 minute delay (?) for theme changes so testing is a pain
+			default: '#fff', 		// primary font color (white)
+			success: '#fff', 		// secondary font color for success background
+			notice: '#fff', 		// secondary font color for notice background
+			warning: '#fff', 		// secondary font color for warning background
+			danger: '#fff', 		// secondary font color for danger background
+			surprise: '#000', 		// secondary font color for surprise background
+			info: '#000' 			// secondary font color for info background
 		},
 		styles: {
 			dialog: {
 				background: {
-					default: '#F0F6FC1A'
+					default: '#21262D' // modal primary background color
+				},
+				foreground: {
+					default: '#000'
 				}
-			},
-			paneHeader: {
-				background: {
-					success: '#1F6FEB',
-					notice: '#F1E05A',
-					warning: '#D2A8FF',
-					danger: '#E34C26',
-					surprise: '#D29922',
-					info: '#58A6FF'
-				}
-			},
+			}
 		}
 	}
 }
