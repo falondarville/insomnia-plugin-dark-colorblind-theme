2eff6259df703b250ae96544ea791610fbbab0e3
Falon Darville <falondarville@gmail.com>
2021-10-01 13:06:31 -0700

    add to README, update default color

--- themes/dark-colorblind.js
+++ themes/dark-colorblind.js
@@ -3,44 +3,44 @@ module.exports = {
 	displayName: 'Dark Colorblind',
 	theme: {
 		background: {
-			default: '#0D1117', 	// primary background color
-			success: '#1F6FEB', 	// POST request, parameter names
-			notice: '#E8F086', 		// SEND button, GET request
-			warning: '#A691AE', 	// PUT request
-			danger: '#FF4242', 		// DELETE request
-			surprise: '#FFC20A', 	// accent (Dashboard link, branch button, add plugin button)
-			info: '#58A6FF' 		// OPTIONS AND HEAD request
+			default:    '#2E4052', 	// primary background color
+			success:    '#1F6FEB', 	// POST request, 200 OK, parameter names
+			notice:     '#E8F086', 	// SEND button, GET request
+			warning:    '#A691AE', 	// PUT request
+			danger:     '#FF4242', 	// DELETE request
+			surprise:   '#FFC20A', 	// accent (Dashboard link, branch button, add plugin button)
+			info:       '#58A6FF' 	// OPTIONS AND HEAD request
 		},
 		foreground: {
 			// font colors corresponding with the elements above?
 			// there's like 5 minute delay (?) for theme changes so testing is a pain
-			default: '#fff', 		// primary font color (white)
-			success: '#fff', 		// secondary font color for success background
-			notice: '#fff', 		// secondary font color for notice background
-			warning: '#fff', 		// secondary font color for warning background
-			danger: '#fff', 		// secondary font color for danger background
-			surprise: '#000', 		// secondary font color for surprise background
-			info: '#000' 			// secondary font color for info background
+			default:     '#fff',    // primary font color (white)
+			success:     '#fff', 	// secondary font color for success background
+			notice:      '#fff', 	// secondary font color for notice background
+			warning:     '#fff', 	// secondary font color for warning background
+			danger:      '#fff', 	// secondary font color for danger background
+			surprise:    '#000', 	// secondary font color for surprise background
+			info:        '#000' 	// secondary font color for info background
 		},
 		styles: {
 			appHeader: {
 				foreground: {
-					surprise: '#000' // branch button font color?
+					surprise:   '#000'      // branch button font color? (black)
 				}
 			},
 			paneHeader: {
 				foreground: {
-					default: '#fff', 	// primary Editor Mode font color (white)
-					surprise: '#000', 	// accent font color
-					info: '#000' 		// response font color
+					default:    '#fff', 	// primary Editor Mode font color (white)
+					surprise:   '#000', 	// accent font color
+					info:       '#000' 		// response font color
 				}
 			},
 			dialog: {
 				background: {
-					default: '#21262D' 	// modal primary background color
+					default:    '#21262D' 	// modal primary background color
 				},
 				foreground: {
-					default: '#fff' 	// primary font color for modals
+					default:    '#fff' 	    // primary font color for modals
 				}
 			}
 		}
